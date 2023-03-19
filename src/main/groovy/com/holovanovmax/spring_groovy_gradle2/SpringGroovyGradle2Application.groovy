c2a517cc8969702c28b4b98cbb5782a5065ed804
maxikyo <holovanovnmax@gmail.com>
2023-03-19 20:05:40 +0100

    first commit

---
+++ src/main/groovy/com/holovanovmax/spring_groovy_gradle2/SpringGroovyGradle2Application.groovy
@@ -0,0 +1,13 @@
+package com.holovanovmax.spring_groovy_gradle2
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringGroovyGradle2Application {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringGroovyGradle2Application, args)
+	}
+
+}
